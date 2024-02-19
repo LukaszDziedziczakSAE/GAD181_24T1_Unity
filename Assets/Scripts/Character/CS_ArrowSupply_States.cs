06f3b7245e61dd8f3ce61b63bc6d73a00f1246a7
SAE1\1031991 <timmeht333@gmail.com>
2024-02-19 10:52:02 +1030

    Scene Commit Arrow Supply

---
+++ Assets/Scripts/Character/CS_ArrowSupply_States.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_ArrowSupply_States : CharacterState
+{
+    public CS_ArrowSupply_States(Character character) : base(character)
+    {
+
+    }
+
+    public override void StateStart()
+    {
+        Debug.Log("Test state working");
+    }
+
+    public override void FixedTick()
+    {
+
+    }
+
+
+    public override void Tick()
+    {
+
+    }
+
+    public override void StateEnd()
+    {
+
+    }
+}
