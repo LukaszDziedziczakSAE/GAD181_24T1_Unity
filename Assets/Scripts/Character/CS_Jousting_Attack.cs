7ebb28ad61ddc3a07f6d24156fc0ed0fb4b93ade
C:\Users\Jack Taeger\Documents\Sourcetree <1032375@student.sae.edu.au>
2024-03-07 20:37:06 +1030

    Added riding while attacking functionality

--- Assets/Scripts/Character/CS_Jousting_Attack.cs
+++ Assets/Scripts/Character/CS_Jousting_Attack.cs
@@ -10,38 +10,47 @@ public class CS_Jousting_Attack : CharacterState
     private UI_Jousting ui;
     private CS_Jousting_Riding riding;
 
-    public bool playerAttacked = false;
+    private bool animationStarted = false; 
+    private float animationDuration; 
 
     public CS_Jousting_Attack(Character character) : base(character)
     {
+        match = (JoustingMatch)Game.Match;
+        other = match.OtherCharacter(character.PlayerIndex);
     }
 
     public override void StateStart()
     {
-        Game.InputReader.OnTouchPressed += InputReader_OnTouchPressed;
-        character.Animator.CrossFade("Attack_Left_Forward_RH", 0.1f);
-        playerAttacked = true;
+        if (character.PlayerIndex == 0 && !animationStarted)
+        {
+            character.Animator.CrossFade("Attack_Left_Forward_RH", 0.1f);
+            animationStarted = true; 
+            animationDuration = character.Animator.GetCurrentAnimatorStateInfo(0).length; 
+        }
 
-        Debug.Log("You've entered attack state");
-    }
-
-    private void InputReader_OnTouchPressed()
-    {
-
+        //Debug.Log("You've entered attack state");
     }
 
     public override void Tick()
     {
-        //character.transform.position += character.transform.forward * match.HorseSpeed * Time.deltaTime;
+        if (animationStarted)
+        {            
+            animationDuration -= Time.deltaTime;
+
+            if (animationDuration <= 0)
+            {
+                character.SetNewState(new CS_Jousting_Riding(character));
+            }
+        }
+        character.transform.position += character.transform.forward * match.HorseSpeed * Time.deltaTime;
     }
 
     public override void FixedTick()
     {
-
     }
 
     public override void StateEnd()
     {
-        Game.InputReader.OnTouchPressed -= InputReader_OnTouchPressed;
+        animationStarted = false; 
     }
 }
