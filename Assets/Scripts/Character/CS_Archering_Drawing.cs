ff91667d6c6a872a3a4ce7eb8bff68b67f69595f
NathanEKelly <1031929@sae.edu.au>
2024-03-10 22:06:41 +1030

    begun arrow shooting mechanic for TargetShooting

--- Assets/Scripts/Character/CS_Archering_Drawing.cs
+++ Assets/Scripts/Character/CS_Archering_Drawing.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CS_Archering_Drawing : CharacterState
 {
     private ArrowShootingMatch match;
     private UI_TargetShooting ui;
+    private TargetShooting_ShootArrow arrow;
     float startingYPostition;
     public CS_Archering_Drawing(Character character) : base(character)
     {
         ui = (UI_TargetShooting)Game.UI;
         match = (ArrowShootingMatch)Game.Match;
+        
     }
-
+    
+    
     public override void StateStart()
     {
         Game.InputReader.OnTouchReleased += InputReader_OnTouchReleased;
         ui.ArrowShootingIndicator.gameObject.SetActive(true);
-        startingYPostition = Game.InputReader.TouchPosition.y;
+        startingYPostition = Game.InputReader.TouchPosition.y;        
         character.Animator.CrossFade("TargetShooting_DrawBlend", 0.1f);
     }
 
@@ -34,9 +38,10 @@ public class CS_Archering_Drawing : CharacterState
             distance = 0;        
         }
         ui.ArrowShootingIndicator.UpdateDrawDistance(distance);
-
         float distanceNormalized = distance / match.MaximumDrawDistanceToFire;
         distanceNormalized = Mathf.Clamp(distanceNormalized, 0, 1);
+        //arrow.ShootArrow(distanceNomalized)
+        
         character.Animator.SetFloat("DrawStrength", distanceNormalized);
     }
     public override void FixedTick()
---
+++ Assets/Scripts/Minigames/TargetShooting/TargetShooting_ShootArrow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetShooting_ShootArrow : ArrowShootingMatch
+{
+
+    [SerializeField] protected GameObject arrowPrefab;
+    [SerializeField] protected Transform arrowFirePoint;
+    float startingYPostition;
+    private ArrowShootingMatch match;
+
+    private void Start()
+    {
+        match = GetComponent<ArrowShootingMatch>();
+    }
+
+    private void Update()
+    {
+        CalculatePower();
+    }
+    void ShootArrow(float releasePower)
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        { 
+            GameObject clone = Instantiate(arrowPrefab, arrowFirePoint.position, Quaternion.identity);
+
+            clone.GetComponent<Rigidbody>().velocity = arrowFirePoint.forward * releasePower*100;
+        }
+    }
+
+    void CalculatePower()
+    {
+        float currentYPosition = Game.InputReader.TouchPosition.y;
+        float distance = startingYPostition - currentYPosition;
+        if (distance < 0)
+        {
+            distance = 0;
+        }
+        
+        float distanceNormalized = distance / match.MaximumDrawDistanceToFire;
+        distanceNormalized = Mathf.Clamp(distanceNormalized, 0, 1);
+
+        ShootArrow(distance);
+    }
+}
