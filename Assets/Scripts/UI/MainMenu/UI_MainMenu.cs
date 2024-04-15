6e2248cbff24bc1d3d7b151ff4b5e3c628c93db6
Lukasz <1032314@student.sae.edu.au>
2024-04-15 11:36:17 +0930

    Added Credits and Survey Link

--- Assets/Scripts/UI/MainMenu/UI_MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/UI_MainMenu.cs
@@ -11,6 +11,7 @@ public class UI_MainMenu : UI_Main
     [field: SerializeField] public UI_PlayerProfile PlayerProfile { get; private set; }
     [field: SerializeField] public UI_CharacterList CharacterList { get; private set; }
     [field: SerializeField] public UI_EnterName EnterNameDialog { get; private set; }
+    [field: SerializeField] public UI_Credits Credits {  get; private set; }
 
     private void OnEnable()
     {
