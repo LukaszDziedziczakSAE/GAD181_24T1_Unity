a2a188eecb102f5ed60135c8debc46ea3f13597b
NathanEKelly <1031929@sae.edu.au>
2024-04-24 10:30:23 +0930

    Started new minigame dangertargetshooting

---
+++ Assets/Scripts/Character/CharacterStates/DangerTargetShooting/CS_DangerTargetShooting_Drawing.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_DangerTargetShooting_Drawing : CharacterState
+{
+    private DangerTargetShooting_Match match;
+    private UI_TargetShooting ui;
+    private TargetShooting_ShootArrow arrow;
+    float startingYPostition;
+    private float maxAngle = 180f;
+    private float startingXPos;
+    private float turnRatio = 20;
+    private float rotationDeadzone = 5f;
+    float characterStartingRotation;
+
+    float currentYPosition => Game.InputReader.TouchPosition.y;
+    float distanceY
+    {
+        get
+        {
+            float distance;
+            if (currentYPosition == 0) distance = startingYPostition - lastYPostion;
+            else distance = startingYPostition - currentYPosition;
+            if (distance < 0) distance = 0;
+            return distance;
+        }
+    }
+    float powerValue
+    {
+        get
+        {
+            float power = distanceY / match.MaximumDrawDistanceToFire;
+            power = Mathf.Clamp(power, 0, 1);
+            return power;
+        }
+    }
+
+    float lastYPostion;
+
+    public CS_DangerTargetShooting_Drawing(Character character) : base(character)
+    {
+        ui = (UI_TargetShooting)Game.UI;
+        match = (DangerTargetShooting_Match)Game.Match;
+
+    }
+
+
+    public override void StateStart()
+    {
+
+        if (IsPlayerCharacter) Game.InputReader.OnTouchReleased += InputReader_OnTouchReleased;
+        if (match.DisplayDebugs) ui.ArrowShootingIndicator.gameObject.SetActive(true);
+        ui.PowerSlider.gameObject.SetActive(true);
+        startingYPostition = Game.InputReader.TouchPosition.y;
+        character.Animator.CrossFade("TargetShooting_DrawBlend", 0.1f);
+
+        if (Game.Player.Level.Level < 7) ui.Crosshair.gameObject.SetActive(true);
+
+        startingXPos = Game.InputReader.TouchPosition.x;
+        characterStartingRotation = character.transform.eulerAngles.y;
+        /*if (characterStartingRotation > 180)
+        {
+            characterStartingRotation -= 360;
+        }*/
+    }
+
+    public override void Tick()
+    {
+        if (!IsPlayerCharacter) return;
+
+        float distanceX = startingXPos - Game.InputReader.TouchPosition.x;
+        if (distanceX > rotationDeadzone || distanceX < -rotationDeadzone)
+        {
+            float rotation = distanceX / turnRatio;
+            rotation += characterStartingRotation;
+            if (rotation > maxAngle) rotation = maxAngle;
+            if (rotation < -maxAngle) rotation = -maxAngle;
+            character.transform.eulerAngles = new UnityEngine.Vector3(character.transform.eulerAngles.x, rotation, character.transform.eulerAngles.z);
+        }
+
+        //ui.ArrowShootingIndicator.UpdateDrawDistance(startingYPostition, currentYPosition, distanceY, powerValue);
+        //ui.ArrowShootingIndicator.UpdateBackgroundColour(distanceY >= match.MinimumDrawDistanceToFire);
+        ui.PowerSlider.UpdateArrowPower(powerValue);
+
+        character.Animator.SetFloat("DrawStrength", powerValue);
+
+        if (currentYPosition != 0) lastYPostion = currentYPosition;
+
+        //Debug.Log("startingYPostition=" + startingYPostition + ", currentYPosition=" + currentYPosition + ", currentYPosition=" + currentYPosition + ", distanceY=" + distanceY);
+    }
+    public override void FixedTick()
+    {
+
+    }
+
+    public override void StateEnd()
+    {
+
+        if (IsPlayerCharacter) Game.InputReader.OnTouchReleased -= InputReader_OnTouchReleased;
+        ui.ArrowShootingIndicator.gameObject.SetActive(false);
+        ui.PowerSlider.gameObject.SetActive(false);
+        ui.Crosshair.gameObject.SetActive(false);
+    }
+
+    private void InputReader_OnTouchReleased()
+    {
+        Debug.Log("END: startingYPostition=" + startingYPostition + ", currentYPosition=" + currentYPosition + ", currentYPosition=" + currentYPosition + ", distanceY=" + distanceY);
+
+        if (distanceY >= match.MinimumDrawDistanceToFire)
+        {
+            Debug.Log(character.name + ": releasing arrow with " + (powerValue * 100).ToString("F0") + "% power");
+            character.SetNewState(new CS_Archering_Releasing(character, powerValue));
+        }
+        else
+        {
+            character.SetNewState(new CS_Archering_Standing(character));
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/DangerTargetShooting/DangerTargetShooting_Arrow.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerTargetShooting_Arrow : MonoBehaviour
+{
+
+    [SerializeField] float baseSpeed = 10;
+    [SerializeField] float timeToLive = 5f;
+    [SerializeField] LayerMask hittableLayers;
+    [SerializeField] float drop = 1f;
+    [SerializeField] int pointPerTargetHit = 1;
+    [SerializeField] int pointPerCrowdHit = 1;
+    [SerializeField] bool dropEnabled;
+    float power = 1f;
+    float birthTime;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] aSounds;
+    //[SerializeField] AudioClip[] bowFlightSounds;
+    float timeAlive => Time.time - birthTime;
+    bool hitSomething;
+    //ArrowShootingMatch match;
+    bool errored;
+    float speed => baseSpeed * power;
+    Character owner;
+    CharacterSounds sound;
+    ArrowShootingMatch match => (ArrowShootingMatch)Game.Match;
+    UI_TargetShooting ui => (UI_TargetShooting)Game.UI;
+    public float Power => power;
+
+    private void Start()
+    {
+        name += Random.Range(100, 1000);
+        //Debug.Log("Arrow Spawned");
+        birthTime = Time.time;
+        //match = (ArrowShootingMatch)Game.Match;
+        sound = FindObjectOfType<CharacterSounds>();
+    }
+
+    private void Update()
+    {
+        if (!hitSomething) transform.position += transform.forward * speed * Time.deltaTime;
+
+        /*if(!hitSomething && owner.PlayerIndex == 0)
+        {
+            PlayArrowFlightSound();
+        }*/
+
+        if (timeAlive >= timeToLive)
+        {
+            Destroy(gameObject);
+            ui.ArrowIndicator.gameObject.SetActive(false);
+        }
+
+        if (dropEnabled && !hitSomething && transform.eulerAngles.x < 90)
+        {
+            float rotation = transform.eulerAngles.x;
+            rotation += drop * Time.deltaTime;
+            transform.eulerAngles = new Vector3(rotation, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        else if (dropEnabled && !hitSomething && transform.eulerAngles.x > 180)
+        {
+            float rotation = transform.eulerAngles.x;
+            rotation += drop * Time.deltaTime;
+            transform.eulerAngles = new Vector3(rotation, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        if (!errored && transform.position.y < 0 && !hitSomething)
+        {
+            errored = true;
+            //Debug.LogError(name + " below ground " + transform.position);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hitSomething = true;
+
+        TargetShooting_Target target = other.GetComponent<TargetShooting_Target>();
+        if (target != null)
+        {
+
+            transform.parent = target.transform;
+            if (target.State == TargetShooting_Target.EState.upPoisition)
+            {
+                target.StartRotatingDown();
+
+                match.AwardPlayerPoints(owner.PlayerIndex, pointPerTargetHit);
+                if (owner.PlayerIndex == 0)
+                {
+                    PlayArrowHitSound();
+                }
+            }
+        }
+
+        Character character = other.GetComponent<Character>();
+        if (character != null && character.State.GetType() != new CS_Death(character).GetType())
+        {
+            transform.parent = character.RootTransform;//i'm pretty sure this is childing correctly but the transform doesnt move with the animation maybe?
+            character.SetNewState(new CS_Death(character));
+            sound.PlayGruntSound();
+        }
+
+
+        float distance = Vector3.Distance(transform.position, owner.transform.position);
+        //Debug.Log(name + " landed " + distance + " away");
+
+    }
+
+    public void Initilise(float newPowerValue, Character character)
+    {
+        transform.parent = null;
+        owner = character;
+        power = newPowerValue;
+        Debug.Log(owner.name + " fired arrow with " + (power * 100).ToString("F0") + "% power");
+
+        if (owner.PlayerIndex == 0 && match.DisplayDebugs)
+        {
+            ui.ArrowIndicator.gameObject.SetActive(true);
+            //ui.ArrowIndicator.UpdateArrowPower(this);
+        }
+    }
+
+    private void PlayAudioClip(AudioClip clip)
+    {
+        if (audioSource == null) return;
+
+        if (audioSource.isPlaying) audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+    public void PlayArrowHitSound()
+    {
+        if (aSounds.Length == 0) return;
+
+        PlayAudioClip(aSounds[Random.Range(0, aSounds.Length)]);
+    }
+}
